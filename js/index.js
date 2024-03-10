739bc21f7ba8849d2f02aac338e652ee6a3bca52
Lasse Christensen <Flas888@gmail.com>
2024-03-10 13:22:33 +0100

    First commit with pictures

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@ function go(loc) {
     document.getElementById('content').src = loc;
 }
 
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#spinner").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#spinner").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
